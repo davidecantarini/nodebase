784ea93341c572e3f9c668a8e72d835644ec7ac4
Davide Cantarini <dav.cantarini@gmail.com>
2025-10-18 15:05:21 +0200

    Carico solo nodebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const page = () => {
-  return <div className = "min-h-screen min-w-screen flex items-center justify-center">
-    <Button variant="outline">
-      Click Mea
-    </Button>
-      </div>;
-}
+const Page = async () => {
+  const users = await prisma.user.findMany();
+  
+return (
+<div>
+    {JSON.stringify(users)}
+</div>
+);
+};
 
-export default page;
+export default Page;
