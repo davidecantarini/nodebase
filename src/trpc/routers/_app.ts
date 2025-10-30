02dca131726ada1d4fe8411e87d88a2b94d3091c
Davide Cantarini <dav.cantarini@gmail.com>
2025-10-30 22:29:00 +0100

    07: ai providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,24 @@
 import { create } from 'domain';
-import { createTRPCRouter, protectedProcedure } from '../init';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { inngest } from '@/inngest/client';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
 export const appRouter = createTRPCRouter({
+
+  testAi: baseProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai"
+    });
+    return {success: true, message: "Job queued"}
+  }),
+
   getWorkflows: protectedProcedure.query(({ ctx }) => {
       return prisma.workflows.findMany();
     }),
-    createWorkflow: protectedProcedure.mutation(async () => {
+  
+  createWorkflow: protectedProcedure.mutation(async () => {
 
     await inngest.send({
       name: "test/hello.world",
